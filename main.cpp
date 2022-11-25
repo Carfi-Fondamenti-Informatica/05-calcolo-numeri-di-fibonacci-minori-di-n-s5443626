56016682eeb5368bcfb166dbc3ecfdf9af1d3451
s5443626 <117823998+s5443626@users.noreply.github.com>
2022-11-25 17:18:50 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+    int n, a=1, b=1;
+    int s;
+    cin >> n;
+    cout << a << endl << b << endl;
+    for (int i=3; i<=n; i++){
+        s=a+b;
+        a=b;
+        b=s;
+        cout << s << endl;
+
+    }
+    return 0;
 }
